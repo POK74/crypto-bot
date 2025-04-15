9400f19b4ed12a3a26fdf3361f6446d0a445ec21
POK74 <139147266+POK74@users.noreply.github.com>
2025-04-15 20:57:49 +0300

    Create data_collector.py

---
+++ data_collector.py
@@ -0,0 +1,4 @@
+async def get_top_100_coins():
+    # Hardkodet liste for testing
+    # I en fullverdig versjon kan denne funksjonen hente data fra CoinMarketCap eller lignende
+    return ["BTC", "ETH", "BNB", "SOL", "ADA"]
