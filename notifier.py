9919946a1ad0f69a1454f81ade5338f226aa46f8
POK74 <139147266+POK74@users.noreply.github.com>
2025-04-25 23:54:57 +0300

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,2 +1,2 @@
-def send_telegram_melding(bot, chat_id, melding):
-    bot.send_message(chat_id=chat_id, text=melding)
+async def send_telegram_melding(bot, chat_id, melding):
+    await bot.send_message(chat_id=chat_id, text=melding)
